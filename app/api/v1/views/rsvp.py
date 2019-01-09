b2619c06b3ff02a741e8f6819ba20c89004eef70
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-10 01:50:45 +0300

    [Feature #163047722] Added the post method

--- app/api/v1/views/rsvp.py
+++ app/api/v1/views/rsvp.py
@@ -15,3 +15,16 @@ api = ResponseModel().api
 
 @api.route('/<int:m_id>/rsvp')
 class ReapondOp(Resource):
+
+    @api.expect(new_res, validate = True)
+    def post(self, m_id):
+        '''Post a question'''
+        a = meetup.get_single_meetup(m_id)
+        if a:
+            new_response = api.payload
+            new_response['r_id'] = len(response.Responds) + 1
+            new_response['meetup_id'] = a['m_id']
+            new_response['topic'] = a['topic']
+            response.Responds.append(new_response)
+            return make_response(jsonify({'Message': "Response added successfully", 'Status': 201, "Data": new_response}), 201)
+        raise NotFound ('Meetup with that id not found')
