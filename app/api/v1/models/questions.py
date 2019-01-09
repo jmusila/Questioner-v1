1369b3cb846680154001fc2fa2a09f7110ee3f1f
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-09 17:24:10 +0300

    [Finishes #163047252] Finishes

--- app/api/v1/models/questions.py
+++ app/api/v1/models/questions.py
@@ -1,1 +1,12 @@
 from datetime import datetime
+
+class Question:
+    """ Questions constructor """
+    def __init__(self, qsn_id, meetup_id, title, body, votes):
+        self.qsn_id = qsn_id
+        self.createdOn = datetime.now()
+        self.meetup_id = meetup_id
+        self.title = title
+        self.body = body
+        self.votes = votes
+        self.Questions = []
