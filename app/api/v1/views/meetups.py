968c1e965d8086a8d0c7ac0b87f03807249fd6cc
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-09 10:51:30 +0300

    [Feature #163047218] Added global variables

--- app/api/v1/views/meetups.py
+++ app/api/v1/views/meetups.py
@@ -6,3 +6,7 @@ from werkzeug.exceptions import NotFound
 #Local imports
 from app.api.v1.models.meetups import Meetups
 from app.api.v1.views.expect import MeetupsModel
+
+meetup = Meetups('m_id', 'location', 'images', 'topic', 'happeningOn')
+new_meetup = MeetupsModel().meetups
+api = MeetupsModel().api
