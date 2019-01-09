88a304ba22220bdffb532d782db75cd8c833502f
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-09 10:50:48 +0300

    [Feature #163047218] Added local imports

--- app/api/v1/views/meetups.py
+++ app/api/v1/views/meetups.py
@@ -2,3 +2,7 @@
 from flask_restplus import Resource
 from flask import request, make_response, jsonify
 from werkzeug.exceptions import NotFound
+
+#Local imports
+from app.api.v1.models.meetups import Meetups
+from app.api.v1.views.expect import MeetupsModel
