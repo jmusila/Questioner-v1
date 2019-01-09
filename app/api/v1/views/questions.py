b8330ef6c7ab295227562e18ef8aa34bef9367f6
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-09 17:14:51 +0300

    [Feature #163047252] Added global varibales

--- app/api/v1/views/questions.py
+++ app/api/v1/views/questions.py
@@ -13,3 +13,7 @@ from app.api.v1.models.questions import Question
 from app.api.v1.views.expect import QuestionModel
 from app.api.v1.models.meetups import Meetups
 from app.api.v1.views.meetups import meetup
+
+question = Question('qsn_id', 'body','meetup_id', 'title', 'votes' )
+new_question = QuestionModel().questions
+api = QuestionModel().api 
