75cc938c28f827050bd29533cd1a9b3ee46e537b
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-10 01:44:31 +0300

    [Feature #163047722] Aligned the data

--- app/api/v1/models/rsvp.py
+++ app/api/v1/models/rsvp.py
@@ -4,8 +4,8 @@ class Responds:
     """ Respond constructor """
 
     def __init__(self, r_id, meetup_id, topic, status):
-    self.r_id = r_id
-    self.meetup_id = meetup_id
-    self.topic = topic
-    self.status = status
-    self.Responds = []
+        self.r_id = r_id
+        self.meetup_id = meetup_id
+        self.topic = topic
+        self.status = status
+        self.Responds = []
