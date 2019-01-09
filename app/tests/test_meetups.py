b1abb13289a8695a93b50ac8996b9b42930512f5
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-10 00:58:44 +0300

    [Chore #163102835] Added test post meetup

--- app/tests/test_meetups.py
+++ app/tests/test_meetups.py
@@ -6,3 +6,8 @@ from app.apps import create_app
 from .base_test import Settings
 
 class TestMeetups(Settings):
+
+    def test_post_meetup(self):
+    """Test API can post a meetup"""
+    res = self.client.post('/meetups/upcoming', content_type='application/json', data=json.dumps(self.meetup))
+    self.assertEqual(res.status_code, 201)
