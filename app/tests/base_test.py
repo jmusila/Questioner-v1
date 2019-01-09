399a398de727abf9031c01a24f1df28e16637f74
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-10 00:52:58 +0300

    [Chore #163102835] Added the test dat

--- app/tests/base_test.py
+++ app/tests/base_test.py
@@ -14,3 +14,12 @@ class Settings(unittest.TestCase):
     def setUp(self):
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client()
+
+        self.meetup ={
+            "m_id":1,
+            "createdOn":"Wed, 09 Jan 2019 22:19:25 GMT",
+            "location": "PAC",
+            "images": "images",
+            "topic": "Python",
+            "happeningOn": "12th Feb"
+        }
