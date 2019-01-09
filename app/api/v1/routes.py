17ddee73e0c0cb105aec46423528dd6bcd8a2bc7
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-09 10:47:52 +0300

    [Feature #163047218] Added the routes

---
+++ app/api/v1/routes.py
@@ -0,0 +1,20 @@
+"""
+This file contains all the routes
+"""
+
+# Third party imports
+from flask import Blueprint
+from flask_restplus import Api
+
+#Local imports
+from .views.meetups import api as meetups_route
+
+version1 = Blueprint('api', __name__, url_prefix="/api/v1")
+api = Api(version1)
+api = Api(
+    title ='Questioner',
+    version='1.0',
+    description='A simple Questioner API',
+)
+
+api.add_namespace(meetups_route, path = "/meetups")
