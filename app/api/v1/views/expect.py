a5316b6a429b2dda4c5d2d98438246aead008c36
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-09 10:57:08 +0300

    [Feature #163047218] Added meetup model

--- app/api/v1/views/expect.py
+++ app/api/v1/views/expect.py
@@ -2,3 +2,18 @@
 
 #Third party imports 
 from flask_restplus import fields, Namespace 
+
+class MeetupsModel:
+    """
+    Meetup input data
+
+    """
+    api = Namespace('Meetups', description = 'Meetups Routes')
+    meetups = api.model('Meetup', {
+        'm_id':fields.Integer(required=True, description='The unique identifier of the meetup'),
+        'createdOn': fields.String(required=True, description='This time the meetup was created'),
+        'location': fields.String(required=True, description='The meetup location'),
+        'images': fields.String(required=True, description='The images attached to a meetup'),
+        'topic': fields.String(required=True, description='The meetup topic'),
+        'happeningOn': fields.String(required=True, description='The date the meetup is happening'),
+    })
