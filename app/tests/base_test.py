490cff05a188feef26ab08f7e50ba502da76a14f
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-10 17:14:43 +0300

    [Chore  #163116717] Added the right data

--- app/tests/base_test.py
+++ app/tests/base_test.py
@@ -14,7 +14,7 @@ class Settings(unittest.TestCase):
     def setUp(self):
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client()
-
+        
         self.meetup ={
             "m_id":1,
             "createdOn":"Wed, 09 Jan 2019 22:19:25 GMT",
