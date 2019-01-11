f3e381b2493f174482fc02d77e37b55bc1f8d202
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-11 23:07:52 +0300

    [Starts #163150002] Refactored meetup models

--- app/api/v1/models/meetups.py
+++ app/api/v1/models/meetups.py
@@ -2,8 +2,7 @@ from datetime import datetime
 
 class Meetups:
     """ Meetups constructor """
-    def __init__(self, m_id, location, images, topic, happeningOn):
-        self.m_id = m_id
+    def __init__(self, location, images, topic, happeningOn):
         self.createdOn = datetime.now()
         self.location = location
         self.images = images
