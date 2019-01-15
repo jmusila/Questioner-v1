5080bfda92f0f144a97a7601476dba3672272891
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-15 11:38:19 +0300

    [Starts #163218183] Added the create meetup method

--- app/api/v1/models/meetups.py
+++ app/api/v1/models/meetups.py
@@ -1,17 +1,29 @@
 from datetime import datetime
 
-class Meetups:
+Meetups = []
+class Meetup:
     """ Meetups constructor """
-    def __init__(self, location, images, topic, happeningOn):
+    def __init__(self, m_id=None, location=None, images=None, topic=None, happeningOn=None):
+        self.m_id = m_id
         self.createdOn = datetime.now()
         self.location = location
         self.images = images
         self.topic = topic
         self.happeningOn = happeningOn
-        self.Meetups = []
+
+    def create_meetup(self, mtup):
+        meetup = {
+            "m_id": mtup['m_id'],
+            "location": mtup['location'],
+            "images": mtup['images'],
+            "topic": mtup['topic'],
+            "happeningOn": mtup['happeningOn'], 
+            "createdOn": str(datetime.now()) 
+        }
+        Meetups.append(meetup)
 
     """ Method for getting a single meetup """
     def get_single_meetup(self, m_id):
-        for m in self.Meetups:
+        for m in Meetups:
             if m['m_id'] == m_id:
                 return m
